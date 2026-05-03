4da9f7902f6827de50ec5b187c5204bb0dd43927
hide87 <hide87@gmail.com>
2026-05-04 00:23:03 +0900

    fx.js 업데이트

--- api/fx.js
+++ api/fx.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’)
 try {
 const KEY = process.env.FINNHUB_API_KEY

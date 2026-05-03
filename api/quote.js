88af901a6ade2d6d0228a30700d149cf1f39d972
hide87 <hide87@gmail.com>
2026-05-04 00:22:47 +0900

    quote.js 업데이트

--- api/quote.js
+++ api/quote.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’)
 const symbols = req.query.symbols || ‘’
 const KEY = process.env.FINNHUB_API_KEY

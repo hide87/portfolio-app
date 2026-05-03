741cc2bb87ccb4b991164fab5469c9ecde2ffd04
hide87 <hide87@gmail.com>
2026-05-03 23:26:11 +0900

    earnings.js 업데이트

--- api/earnings.js
+++ api/earnings.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’)
 const { ticker } = req.query
 if (!ticker) return res.status(400).json({ error: ‘No ticker’ })
